2e55b5d49f047b716e8626467dd43707128e69f2
Ankur Ranpariya <patelankurv2005@gmail.com>
2023-08-14 14:12:10 +0530

    pagination added

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     return render(request, "index.html")
@@ -15,10 +15,20 @@ def detail(request):
     return render(request, "detail.html")
 
 def shop(request):
-    product = Product.objects.all()
-    p = Paginator(Product.objects.all(),3)
-    print(p)
-    context = {'product':product}
+    product = Product.objects.all().order_by('id')
+    p = Paginator(product,3)
+    page_number = request.GET.get('page')
+    print(page_number,"@@@@@@@@@@@@@@@@@@@@@")
+    
+
+    try:
+        pagination = p.page(page_number)
+    except PageNotAnInteger:
+        pagination = p.page(1)
+    except EmptyPage:
+        pagination = p.page(p.num_pages)
+    
+    context = {'product':pagination}
     return render(request, "shop.html",context)
 
 def signup(request):
